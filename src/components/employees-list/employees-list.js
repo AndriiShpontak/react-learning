5d53bb0cdfd6028fa19582fad55e7236bed44572
AndriiSH <andriishpontak@gmail.com>
2022-09-13 16:54:56 +0300

    the practice of states on the project, working with forms, managed and unmanaged components, state immutability and custom events

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -2,12 +2,15 @@ import EmployyesListItem from '../employees-list-item/employees-list-item';
 
 import './employees-list.css';
 
-const EmpoyeesList = ({data}) => {
+const EmpoyeesList = ({data, onDelete}) => {
 
     const elements = data.map(item => {
         const {id, ...itemProps} = item;
         return (
-            <EmployyesListItem key={id} {...itemProps}/>
+            <EmployyesListItem 
+                key={id} 
+                {...itemProps}
+                onDelete={() => onDelete(id)} />
         )
     })
 
